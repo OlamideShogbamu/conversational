e37f0e89a7beffbec2444838d6e0d266db3a0b9c
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2024-06-01 05:54:58 +0100

    build: update deploy_to_herok.sh

--- deploy_to_heroku.sh
+++ deploy_to_heroku.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Build the Docker image
-docker build -t registry.heroku.com/conversational-bot-app/web .
+docker build -t registry.heroku.com/registry.heroku.com/dry-peak-46846/web .
 
 # Push the Docker image to Heroku
-docker push registry.heroku.com/conversational-bot-app/web
+docker push registry.heroku.com/registry.heroku.com/dry-peak-46846/web
